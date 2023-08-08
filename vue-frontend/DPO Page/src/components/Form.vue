fd3ecdc3c601ccb8b0ca823e90b1cf96965408b8
Hank Richter <hankrichter97@gmail.com>
2023-08-08 10:04:31 -0400

    Form component

---
+++ vue-frontend/DPO Page/src/components/Form.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Form Component</h1>
+    <form @submit.prevent="submitForm">
+      <input type="text" v-model="formData.name" placeholder="Name" /><br />
+      <input type="email" v-model="formData.email" placeholder="Email" /><br />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Form",
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      axios.post("http://localhost:3000/submit", this.formData)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
